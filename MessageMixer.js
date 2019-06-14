7b049fe83a14bfeda83ff1fab2f3ebaa909ac405
tsinat08 <tsinat.weldetnesae@gmail.com>
2019-06-14 16:30:47 -0400

    first entry

--- MessageMixer.js
+++ MessageMixer.js
@@ -1,3 +1,5 @@
+
+const MessageMixer = {};
 function countCharacter(inputString, inputCharacter) {
     let count = 0;
     let string = inputString.toLowerCase();
@@ -51,13 +53,3 @@ function encode(string) {
 };
 
 
-function displayMessage() {
-    console.log(countCharacter("What is the color of the sky?", "t"));
-    console.log(capitalizeFirstCharacterOfWords("What is the color of the sky?"));
-    console.log(reverseWord("What is the color of the sky?"));
-    console.log(reverseAllWords("What is the color of the sky?"));
-    console.log(replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
-    console.log(encode("What is the color of the sky?"));
-}
-
-displayMessage();
